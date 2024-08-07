20bbc270077e9832be62d7a6b9574aea5ae9adb8
kel-github <getkellygarner@gmail.com>
2024-08-07 12:02:34 +1000

    finished se_cont.R

--- R/se_cont.R
+++ R/se_cont.R
@@ -1,3 +1,24 @@
+#' Compute the standard error of a contrast
+#'
+#' This computes the estimated standard error of
+#' a contrast, given the mean square error (MSE),
+#' the contrast vector, and the relevant n.
+#'
+#' See the definition of sigma from equation 3 of
+#' Bird (2002), p. 203
+#'
+#' @param MSE mean square error: a single numeric value
+#' @param contrast_vector a vector defining the contrast
+#' @param n n observations: a single numeric value
+#'
+#' @return SE: a single numeric value
+#' @export
+#'
+#' @examples
+#' x <- 2.2
+#' y <- c(1, -1)
+#' z <- 50
+#' se_cont(x, y, z)
 se_cont <- function(MSE, contrast_vector, n){
   # compute the standard error of the contrast
   # --
