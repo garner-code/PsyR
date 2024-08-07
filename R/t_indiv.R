76a0430ae0260d0aa774d75c3ba3ec3ff0888df5
kel-github <getkellygarner@gmail.com>
2024-08-07 14:34:25 +1000

    documented cc_individual

--- R/t_indiv.R
+++ R/t_indiv.R
@@ -14,15 +14,19 @@
 #' See Bird (2002) https://doi.org/10.1177/0013164402062002001 p. 205.
 #'
 #' @param v_e single value - residual degrees of freedom from the ANOVA model
-#' @param alpha single value - alpha rate (acceptable rate of type 1 error)
+#' @param alpha single value - alpha rate (acceptable rate of type 1 error).
+#'                Default = 0.05.
 #'
 #' @return a single value - the critical constant (as referenced in eq 3 of
 #'            Bird (2002))
 #' @export
 #'
 #' @examples
-#' fit <- aov(yield~group*spacing+Error(subj/spacing), data=spacing)
-#' df = 12 # degrees of freedom from the
+#' N = 16 # total sample size
+#' J = number of groups
+#' v_e = N - J
+#' alpha = 0.05
+#' cc_individual(v_e, alpha)
 cc_individual <- function(v_e, alpha = 0.05){
 
   qt(alpha/2, v_e, lower.tail = FALSE)
