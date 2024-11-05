2d73e83d0ddd16c95548a9e9bb14d596a8854df9
kelly <getkellygarner@gmail.com>
2024-11-05 13:05:05 +1100

    finished ph_bw function, halfway through user function

---
+++ R/cc_ph_bw.R
@@ -0,0 +1,35 @@
+#' Critical constant for post-hoc simultaneous between x within CIs
+#'
+#' Computes the critical constant for use in calculating post-hoc simultaneous
+#' confidence intervals for between x within contrast effects. For further
+#' details see equation 9 (p. 217) from Bird (2002),
+#' https://doi.org/10.1177/0013164402062002001.
+#'
+#' @param v_w a single numeric value - df within
+#' @param v_b a single numeric value - df between
+#' @param v_e a single numeric value - df error
+#' @param alpha desired type 1 error rate e.g. p=.05
+#'
+#' @return a single numeric value that is the critical constant for use in the
+#' construction of post-hoc simultaneous confidence intervals for between x
+#' within subject effects
+#' @export
+#'
+#' @examples
+#' N = 16 # total number of subjects
+#' W = 3 # number of levels within
+#' J = 4 # number of groups
+#' v_w = W - 1
+#' v_b = J - 1
+#' v_e = N - J
+#' alpha = 0.05
+#' cc_ph_bw(v_w, v_b, v_e, alpha)
+cc_ph_bw <- function(v_w, v_b, v_e, alpha){
+
+  s = min(v_b, v_w)
+  m = (abs(v_b - v_e)-1)/2
+  n = (v_e - v_w - 1)/2
+
+  theta = gcr_crit(alpha, s, m, n)
+  sqrt((v_e*theta)/(1-theta))
+}
---
+++ R/psyci.R
@@ -0,0 +1,52 @@
+psyci <- function(contrast_table, method, family = NA, alpha = 0.05){
+
+  if (!class(contrast_table)[[1]] == "emmGrid" |
+        !class(contrast_table)[[1]] == "summary_emm"){
+    stop("Error: contrast table needs to be of class emmGrid or summary_emm")
+  }
+  if (!method %in% c("ind", "bf", "ph")){
+    stop("Error: method should be ind, bf, or ph")
+  }
+  if (method == "ph" & is.na(family)){
+    stop("Error: define family if using ph method (b, w, or bw)")
+  }
+  if (method != "ph" & !is.na(family)){
+    warning("Warning: family is defined but not with ph method.
+            Ignoring family.")
+  }
+  if (!is.na(family) & !family %in% c("b", "w", "bw")){
+    stop("Error: family should be b, w, or bw")
+  }
+
+  contrast_table = summary(contrast_table)
+  v_e = contrast_table$df[1]
+  if (method %in% "ind"){
+
+    critical_constant = cc_individual(v_e=v_e)
+  } else if (method %in% "bf") {
+
+    nk = nrow(contrast_table)
+    crtical_constant = cc_bonf_t(v_e=v_e, n_k=nk, alpha=alpha)
+  } else if (method %in% "ph"){
+
+    if (family %in% "b"){
+
+      v_b = # this needs to be defined, ideally within the code
+      critical_constant = cc_ph_b(v_b=v_b, v_e=v_e, alpha=alpha)
+
+    } else if (family %in% "w") {
+
+      v_e = # this needs to be defined, ideally within the code
+      critical_constant = cc_ph_w(v_w=v_w, v_e=v_e, alpha=alpha)
+
+    } else if (family %in% "bw") {
+
+    }
+  }
+
+  se_cont = contrast_table[,"SE"]
+  cis = unlist(lapply(se_cont, contrast_ci, critical_constant))
+  contrast_table$lower = contrast_table$estimate - cis
+  contrast_table$upper = contrast_table$estimate + cis
+  contrast_table
+}
