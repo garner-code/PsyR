bcdcf6f682a7682c1c85fe917442bae88bc0d85c
kelly <getkellygarner@gmail.com>
2024-11-06 10:02:07 +1100

    added get_levels to count levels in a factor

---
+++ R/get_levels.R
@@ -0,0 +1,24 @@
+#' get number of levels in a factor
+#'
+#' from a long form data frame, get the number of levels
+#' in the factor specified by factor
+#' @param data a longform dataframe. Predictor variables should be factors
+#' @param factor a single string which matches the name of the factor in the
+#' dataframe
+#'
+#' @return a single numeric value which is the number of levels in factor fctr
+#' @export
+#'
+#' @examples
+#' data = data.frame(A = c("X", "X", "X", "Y", "Y", "Y"))
+#' data$A <- as.factor(data$A)
+#' fctr = "A"
+#' get_levels(data, fctr)
+#'
+get_levels <- function(data, fctr){
+  if (!is.factor(data[,fctr])){
+    stop(sprintf("Error: '%s' is not of type factor", fctr))
+  }
+
+  length(levels(data[,fctr]))
+}
