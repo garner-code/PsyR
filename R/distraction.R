523693d1fbb82f3a50191af08adeb431c310d247
Kelly Garner <kgarner@ad.life.unsw.edu.au>
2026-02-25 16:52:54 +1100

    up to sorting out critical value when also performing simple fx

---
+++ R/distraction.R
@@ -0,0 +1,13 @@
+#' data set looking at performance under different distraction conditions
+#'
+#' @format ## `distraction`
+#' A data frame of simulated data containing 90 rows and 4 columns
+#' \describe{
+#'   \item{Subject}{Subject ID}
+#'   \item{Auditory}{Level of Auditory distraction; Low, Medium, or High}
+#'   \item{Visual}{Level of Visual distraction; None, Dissimilar, Similar
+#'   \item{Performance}{Performance Score}
+#'   ...
+#' }
+#' @source Dr. Melanie Gleitzman
+"distraction"
